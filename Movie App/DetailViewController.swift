5a5726d3c553ab7e50aef1e20a874fb3adf381ce
dionyysus <gizemcsk1@gmail.com>
2023-03-18 11:08:02 +0300

    add to favorites button added, detail controller updated

--- Movie App/DetailViewController.swift
+++ Movie App/DetailViewController.swift
@@ -9,21 +9,18 @@ import UIKit
 
 class DetailViewController: UIViewController {
 
+    @IBOutlet var movieImage: UIImageView!
+    @IBOutlet var movieName: UILabel!
+    @IBOutlet var movieRating: UILabel!
+    @IBOutlet var movieTime: UILabel!
+    @IBOutlet var movieSynopsis: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func addToFavoritesButton(_ sender: Any) {
     }
-    */
-
 }
